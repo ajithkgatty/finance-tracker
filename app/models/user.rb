522ad94aee8a01c60c82986f38129743d1af5475
Ajith <ajithkgatty@gmail.com>
2017-10-20 11:17:52 +0530

    remove friendship and adding the user details

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
   end
 
   def full_name
-    return "#{first_name}#{last_name}".strip if ( first_name || last_name )
+    return "#{first_name} #{last_name}".strip if ( first_name || last_name )
     "Annonymous"
   end
 
